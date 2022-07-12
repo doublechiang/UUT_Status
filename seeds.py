73e103a933d3b13bdd2f923cad973d460d0da1b8
Jiangjunyu <double.chiang@gmail.com>
2022-07-12 14:14:30 -0700

    seeds helper application done.

--- seeds.py
+++ seeds.py
@@ -2,25 +2,25 @@
 
 import os
 import test_station
-import tm
+# import tm
 import logging
 
-# tss = "log@192.168.0.130 log@192.168.0.83".split()
-tss = "log@192.168.0.210 log@192.168.0.130 log@192.168.0.213 log@192.168.0.212 log@192.168.0.211 log@192.168.0.214 log@192.168.0.215".split()
-hop = "cchiang@192.168.66.108"
+
+import settings
+
+""" This file is used for development purpose.
+    Copy the seeds data from PXE test station accroding to the settings
+"""
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
 
-    # if flask_env env is development will not sync
+    # unset the development mode, if flask_env env is development will not sync
     if os.getenv('FLASK_ENV') == 'development':
         del os.environ['FLASK_ENV']
 
-    prjs = tm.TestMonitor.getSupportedPrj()
-    print(prjs)
-
-    for ts in tss:
-        t = test_station.TestStation(ts)
-        t.sync(prjs, hop=hop)
+    for ts in settings.tss:
+        t = test_station.TestStation(**ts)
+        t.sync(hop=settings.hop_station)
         
 
